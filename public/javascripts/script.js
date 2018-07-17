3019a4876c86290338f9e5f7a9d5ed64f1c1e531
소정 <sjk1708@gmail.com>
2018-07-17 13:52:32 +0900

    swipe test

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
 
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
+  
   console.log("readyjs");
   var image = [
     "q1", "q2", "q102",
