fe24ca7df78be5d5686b9ba1a27c8174362b05d4
소정 <sjk1708@gmail.com>
2018-07-17 14:13:20 +0900

    swipe test

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   //initialize swiper when document ready
   var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizonal',
     loop: true
   })
 
